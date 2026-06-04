10d9494508e55e5a1d63bacde3cb490d59d8df4f
Vijaya Baskar P <vbluben@gmail.com>
2026-06-04 19:44:18 +0530

    feat: initialize Express server with Clerk authentication and Inngest middleware integration

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors({origin:"http://localhost:5173"}));
+app.use(cors({origin:process.env.FRONTEND_URL}));
 app.use(clerkMiddleware());
 
 app.use("api/inngest",serve({client:inngest,functions}))
