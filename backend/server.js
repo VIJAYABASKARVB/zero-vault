69b45493b95129f92cd9d24a211c8f22d2519d10
Vijaya Baskar P <vbluben@gmail.com>
2026-06-05 11:30:55 +0530

    feat: implement Clerk authentication with protected backend route and frontend integration

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./src/config/db.js"
-import { clerkMiddleware,clerkClient,getAuth} from '@clerk/express'
+import { clerkMiddleware,requireAuth,getAuth} from '@clerk/express'
 import cors from "cors";
 import { serve } from "inngest/express";  
 import { inngest, functions } from "./src/config/inngest.js"; 
@@ -18,26 +18,16 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 
 connectDB();
 
-app.get('/protected',async (req,res) => {
-  const auth = getAuth(req);
-
-  if (!auth.isAuthenticated) {
-    return res.status(401).json({
-      message: "Unauthorized",
-    });
-  }
-
-  res.json({
-    message: "Protected Route",
-    userId: auth.userId,
-  });
-})
-
 
 app.get('/',(req,res)=>{
   res.send("Hello World");
 });
 
+// Apply middleware to a specific route
+// Redirects to the homepage if the user is not authenticated
+app.get('/protected', requireAuth(), (req, res) => {
+  res.send('This is a protected route.')
+})
 
 app.listen(3000,() => {
   console.log(`Server is running on port : http://localhost:3000`);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
 import './App.css'
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import { Show, SignInButton, SignUpButton, UserButton,useAuth} from '@clerk/react'
 
 function App() {
+
+  const {getToken} = useAuth();
+
+  const testProtectedRoute = async () =>{
+    const token = await getToken();
+    const res = await fetch('https://your-backend-domain.vercel.app/protected',
+                           {headers:{Authorization:`Bearer ${token}`}
+    });
+
+    const data = await res.text();
+    alert(data);
+  }
+  
   return (
     <>
       <header>
@@ -11,6 +24,7 @@ function App() {
         </Show>
         <Show when="signed-in">
           <UserButton />
+          <button onClick={testProtectedRoute}>Test Protected Route</button>
         </Show>
       </header>
     </>
